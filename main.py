55965c730b96b5a55a5514ab922a15161372cd56
Chris Overly <cqoverly@localhost.localdomain>
2021-02-07 19:46:39 -0800

    Selecting from contact list now loads data for the selected contact.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import sys
 import os
 
-from PySide2 import QtWidgets as qtw    
+from PySide2 import QtWidgets as qtw
 from PySide2 import QtGui as qtg
 from PySide2 import QtCore as qtc
 from PySide2 import QtUiTools as qtu
@@ -10,10 +10,12 @@ from PySide2 import QtUiTools as qtu
 import database as db
 
 
-logger = logging.getLogger('app_logger')
+logger = logging.getLogger("app_logger")
 logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(process)d - %(levelname)s - %(message)s')
+    level=logging.DEBUG, format="%(process)d - %(levelname)s - %(message)s"
+)
+
+list_dict = {}
 
 
 class MainInterface(qtw.QMainWindow):
@@ -26,79 +28,109 @@ class MainInterface(qtw.QMainWindow):
         self.window = loader.load(ui_file)
         ui_file.close()
 
+        self.list_dict = {}  # for keeping track of contact_id's for list names
+
         # Bring in interface widgets for use
 
         # Labels
-        self.lbl_full_name = self.window.findChild(qtw.QLabel, 'lbl_full_name')
-        self.lbl_company = self.window.findChild(qtw.QLabel, 'lbl_company')
+        self.lbl_full_name = self.window.findChild(qtw.QLabel, "lbl_full_name")
+        self.lbl_company = self.window.findChild(qtw.QLabel, "lbl_company")
 
         # Listviews
-        self.lw_contact_list = self.window.findChild(qtw.QListWidget, 'lw_contact_list')
-        self.lw_contact_list.itemSelectionChanged.connect(self.get_item())
+        self.lw_contact_list = self.window.findChild(qtw.QListWidget, "lw_contact_list")
+        self.lw_contact_list.itemClicked.connect(self.load_contact)
 
         # Line Edits
-        self.le_last_name = self.window.findChild(qtw.QLineEdit, 'le_last_name')
-        self.le_first_name = self.window.findChild(qtw.QLineEdit, 'le_first_name')
-        self.le_company = self.window.findChild(qtw.QLineEdit, 'le_company')
-        self.le_email = self.window.findChild(qtw.QLineEdit, 'le_email')
-        self.le_home_phone = self.window.findChild(qtw.QLineEdit, 'le_home_phone')
-        self.le_work_phone = self.window.findChild(qtw.QLineEdit, 'le_work_phone')
+        self.le_last_name = self.window.findChild(qtw.QLineEdit, "le_last_name")
+        self.le_first_name = self.window.findChild(qtw.QLineEdit, "le_first_name")
+        self.le_company = self.window.findChild(qtw.QLineEdit, "le_company")
+        self.le_email = self.window.findChild(qtw.QLineEdit, "le_email")
+        self.le_home_phone = self.window.findChild(qtw.QLineEdit, "le_home_phone")
+        self.le_work_phone = self.window.findChild(qtw.QLineEdit, "le_work_phone")
 
         # Text Edits
-        self.te_notes = self.window.findChild(qtw.QTextEdit, 'te_notes')
+        self.te_notes = self.window.findChild(qtw.QTextEdit, "te_notes")
 
         # Buttons
-        self.btn_new_contact = self.window.findChild(qtw.QPushButton,
-                                            'btn_new_contact')
+        self.btn_new_contact = self.window.findChild(qtw.QPushButton, "btn_new_contact")
+        self.btn_new_contact.clicked.connect(self.add_contact)
 
         self.load_interface()
 
         self.window.show()
 
-    
     def load_interface(self):
-        '''
-            list is return from db.read_db() as follows:
-            (
-                contact_id,
-                last_name,
-                first_name,
-                company,
-                email,
-                home_phone,
-                work_phone,
-                notes
-            )
-        '''
+        """
+        list is return from db.read_db() as follows:
+        (
+            contact_id,
+            last_name,
+            first_name,
+            company,
+            email,
+            home_phone,
+            work_phone,
+            notes
+        )
+        """
 
-        logger.info('Loading interface')
+        logger.info("Loading interface")
         contacts = db.read_db()
-        if contacts[0][1] == 'last_name':
+
+        # Remove column headers if in list of contacts
+        if contacts[0][1] == "last_name":
             contacts.pop(0)
         contact_names = []
-        list_dict = {}
+
         for c in contacts:
-            list_name = f'{c[1]}, {c[2]}'
+            # Cycle through contacts and generate a dictionary
+            # of full names, to match listwidget items, and the
+            # contact_id associated for that name for
+            # database lookup.
+            list_name = f"{c[1]}, {c[2]}"
             contact_names.append(list_name)
             list_dict[list_name] = c[0]
+
         contact_names.sort()
         self.lw_contact_list.addItems(contact_names)
         self.lw_contact_list.setCurrentRow(0)
         c_id = list_dict[contact_names[0]]
         initial_contact = db.get_contact(c_id)
-        self.lbl_full_name.setText(f'{initial_contact[2]} {initial_contact[1]}')
+
+        # set fields in ui
+        self.lbl_full_name.setText(f"{initial_contact[2]} {initial_contact[1]}")
+        self.lbl_company.setText(initial_contact[3])
+        self.le_last_name.setText(initial_contact[1])
+        self.le_first_name.setText(initial_contact[2])
+        self.le_company.setText(initial_contact[3])
+        self.le_email.setText(initial_contact[4])
+        self.le_home_phone.setText(initial_contact[5])
+        self.le_work_phone.setText(initial_contact[6])
+        self.te_notes.setText(initial_contact[7])
 
         logger.info(initial_contact)
 
+    def load_contact(self, item):
+        contact = db.get_contact(list_dict[item.text()])
+        self.lbl_full_name.setText(f"{contact[2]} {contact[1]}")
+        self.lbl_company.setText(contact[3])
+        self.le_last_name.setText(contact[1])
+        self.le_first_name.setText(contact[2])
+        self.le_company.setText(contact[3])
+        self.le_email.setText(contact[4])
+        self.le_home_phone.setText(contact[5])
+        self.le_work_phone.setText(contact[6])
+        self.te_notes.setText(contact[7])
+        logger.info(contact)
 
-    def get_item(self):
-        item = self.lw_contact_list.currentItem()
-        print(item)
+    def add_contact(self):
+        print("Add Contact")
 
 
 if __name__ == "__main__":
     import sys
+
     qtc.QCoreApplication.setAttribute(qtc.Qt.AA_ShareOpenGLContexts)
     app = qtw.QApplication(sys.argv)
-    MainWindow = MainInterface('interface.ui')
+    MainWindow = MainInterface("interface.ui")
     sys.exit(app.exec_())
