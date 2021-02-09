d3def987247ddd5198cdce94177c32d9d3788c6d
Chris Overly <cqoverly@gmail.com>
2021-02-08 22:34:57 -0800

    can add and edit contacts. Problem exists when reloading interface after save. New contacts do not show up until program is restarted.

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ logging.basicConfig(
     level=logging.DEBUG, format="%(process)d - %(levelname)s - %(message)s"
 )
 
-list_dict = {}
 
 
 class MainInterface(qtw.QMainWindow):
@@ -29,7 +28,7 @@ class MainInterface(qtw.QMainWindow):
         ui_file.close()
 
         self.list_dict = {}  # for keeping track of contact_id's for list names
-
+        self.save_contact_id:int = None  # to save which contact to save to.
         # Bring in interface widgets for use
 
         # Labels
@@ -56,14 +55,18 @@ class MainInterface(qtw.QMainWindow):
         self.btn_new_contact.clicked.connect(self.new_contact)
         self.btn_save = self.window.findChild(qtw.QPushButton, 'btn_save')
         self.btn_save.clicked.connect(self.save_contact)
+        self.btn_edit = self.window.findChild(qtw.QPushButton, 'btn_edit')
+        self.btn_edit.clicked.connect(self.edit_contact)
 
         self.load_interface()
+        self.lw_contact_list.setCurrentRow(0)
 
         self.window.show()
 
     def load_interface(self):
         """
-        list is return from db.read_db() as follows:
+        load_interface calls db.read_db() to collect contact data from the 
+        database. A list is returned from db.read_db() as follows:
         (
             contact_id,
             last_name,
@@ -74,6 +77,8 @@ class MainInterface(qtw.QMainWindow):
             work_phone,
             notes
         )
+
+
         """
 
         logger.info("Loading interface")
@@ -91,12 +96,11 @@ class MainInterface(qtw.QMainWindow):
             # database lookup.
             list_name = f"{c[1]}, {c[2]}"
             contact_names.append(list_name)
-            list_dict[list_name] = c[0]
+            self.list_dict[list_name] = c[0]
 
         contact_names.sort()
         self.lw_contact_list.addItems(contact_names)
-        self.lw_contact_list.setCurrentRow(0)
-        c_id = list_dict[contact_names[0]]
+        c_id = self.list_dict[contact_names[0]]
         initial_contact = db.get_contact(c_id)
 
         # set fields in ui
@@ -113,7 +117,9 @@ class MainInterface(qtw.QMainWindow):
         logger.info(initial_contact)
 
     def load_contact(self, item):
-        contact = db.get_contact(list_dict[item.text()])
+        self.save_contact_id = self.list_dict[item.text()]
+        print(self.save_contact_id)
+        contact = db.get_contact(self.list_dict[item.text()])
         self.lbl_full_name.setText(f"{contact[2]} {contact[1]}")
         self.lbl_company.setText(contact[3])
         self.le_last_name.setText(contact[1])
@@ -123,39 +129,54 @@ class MainInterface(qtw.QMainWindow):
         self.le_home_phone.setText(contact[5])
         self.le_work_phone.setText(contact[6])
         self.te_notes.setText(contact[7])
+        self.disable_editing()
         logger.info(contact)
 
-    def new_contact(self):
-        print('NEW')
+    def enable_editing(self):
         self.btn_save.setEnabled(True)
+        self.le_last_name.setReadOnly(False)
+        self.le_first_name.setReadOnly(False)
+        self.le_company.setReadOnly(False)
+        self.le_email.setReadOnly(False)
+        self.le_home_phone.setReadOnly(False)
+        self.le_work_phone.setReadOnly(False)
+        self.te_notes.setReadOnly(False)
+
+    def disable_editing(self):
+        self.btn_save.setEnabled(False)
+        self.le_last_name.setReadOnly(True)
+        self.le_first_name.setReadOnly(True)
+        self.le_company.setReadOnly(True)
+        self.le_email.setReadOnly(True)
+        self.le_home_phone.setReadOnly(True)
+        self.le_work_phone.setReadOnly(True)
+        self.te_notes.setReadOnly(True)
+
+    def new_contact(self):
+        self.save_contact_id = None
+        self.enable_editing()
         self.lbl_full_name.setText("")
         self.lbl_company.setText("")
         self.le_last_name.setText("")
-        self.le_last_name.setReadOnly(False)
         self.le_first_name.setText("")
-        self.le_first_name.setReadOnly(False)
         self.le_company.setText("")
-        self.le_company.setReadOnly(False)
         self.le_email.setText("")
-        self.le_email.setReadOnly(False)
         self.le_home_phone.setText("")
-        self.le_home_phone.setReadOnly(False)
         self.le_work_phone.setText("")
-        self.le_work_phone.setReadOnly(False)
         self.te_notes.setText("")
     
     def save_contact(self):
-        last = self.le_last_name.text
-        first = self.le_first_name.text
-        company = self.le_company.text
-        email = self.le_email.text
-        home_phone = self.le_home_phone.text
-        work_phone = self.le_work_phone.text
-        notes = self.te_notes.text
+        last = self.le_last_name.text()
+        first = self.le_first_name.text()
+        company = self.le_company.text()
+        email = self.le_email.text()
+        home_phone = self.le_home_phone.text()
+        work_phone = self.le_work_phone.text()
+        notes = self.te_notes.toPlainText()
 
         valid = False
 
-        params = (
+        params = [
             last,
             first,
             company,
@@ -163,15 +184,28 @@ class MainInterface(qtw.QMainWindow):
             home_phone,
             work_phone,
             notes
-        )
+        ]
 
-        for p in params:
+        for p in params[:3]:
             if len(p.strip()) != 0:
                 valid = True
         if valid:
-            db.add_contact(*params)
+            if self.save_contact_id:
+                params.insert(0, self.save_contact_id)
+                db.update_contact(params)
+                self.btn_save.setEnabled(False)
+            else:
+                db.add_contact(*params)
+                self.btn_save.setEnabled(False)
+                self.load_interface()
         else:
+            logger.warning('Did not save contact. Contact must have either last of first name, or company name.')
             pass
+
+    def edit_contact(self):
+        print(self.save_contact_id)
+        self.enable_editing()
+        pass
 
 
 if __name__ == "__main__":
